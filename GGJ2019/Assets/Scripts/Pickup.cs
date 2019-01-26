588e92ba677a3f93e9ea73084b56cab3a2333a39
Eric Ling <eric@playstudios.asia>
2019-01-27 03:46:39 +0800

    Add sfx

--- GGJ2019/Assets/Scripts/Pickup.cs
+++ GGJ2019/Assets/Scripts/Pickup.cs
@@ -3,25 +3,33 @@ using System.Collections.Generic;
 using DefaultNamespace;
 using UnityEngine;
 
-public class Pickup : MonoBehaviour {
+public class Pickup : MonoBehaviour
+{
+    public AudioClip Clip;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	
-	
-	private void OnTriggerEnter(Collider other) {
-		Debug.Log("Enter Pickup");
-		if (other.gameObject.GetComponent<PlayerController>() != null) {
-			Debug.Log("Do pickup");
-			EventManager.TriggerEvent(GameEvent.PICKUP, new EventParam());
-			transform.gameObject.SetActive(false);
-		}
-	}
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enter Pickup");
+        if (other.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Debug.Log("Do pickup");
+            EventManager.TriggerEvent(GameEvent.PICKUP, new EventParam());
+            transform.gameObject.SetActive(false);
+            var hudAudio = GameObject.Find("HUD").GetComponent<AudioSource>();
+            
+            hudAudio.clip = this.Clip;
+            hudAudio.Play();
+        }
+    }
 }
