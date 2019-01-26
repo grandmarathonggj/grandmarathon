583c7acf8ceb454a75a7c261cb4225e870862015
Eric Ling <eric@playstudios.asia>
2019-01-27 03:13:12 +0800

    Add pickup

---
+++ GGJ2019/Assets/Scripts/Pickup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	
+	private void OnTriggerEnter(Collider other) {
+		Debug.Log("Enter Pickup");
+		if (other.gameObject.GetComponent<PlayerController>() != null) {
+			Debug.Log("Do pickup");
+			EventManager.TriggerEvent(GameEvent.PICKUP, new EventParam());
+			transform.gameObject.SetActive(false);
+		}
+	}
+}
