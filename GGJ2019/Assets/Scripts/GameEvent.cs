985d19ac345927225170dd7336a2ca5f7b86212c
Eric Ling <eric@playstudios.asia>
2019-01-26 14:02:44 +0800

    Add time's up event

--- GGJ2019/Assets/Scripts/GameEvent.cs
+++ GGJ2019/Assets/Scripts/GameEvent.cs
@@ -2,6 +2,7 @@ namespace DefaultNamespace
 {
     public class GameEvent
     {
+        public static string LEVEL_TIMER_END="LEVEL_TIMER_END";
         public const string START_LEVEL_TIMER = "START_LEVEL_TIMER";
         public const string LEVEL_TIMER_TICK = "LEVEL_TIMER_TICK";
     }
