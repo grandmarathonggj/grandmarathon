7ecc50007f5a3575b0a905b088fa4c80abba8549
Rock <rockcheung2004@yahoo.com.hk>
2019-01-26 12:10:48 +0800

    Added level structures

---
+++ GGJ2019/Assets/Scripts/EndPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPoint : MonoBehaviour {
+	private Level _level;
+	// Use this for initialization
+	void Start () {
+		_level = GameObject.FindObjectOfType<Level>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.gameObject.name == "Player") {
+			_level.TriggerWinScene();
+		}
+	}
+}
