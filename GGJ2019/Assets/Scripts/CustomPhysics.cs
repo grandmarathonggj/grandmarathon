936f0c49b757a7041147bd67983e96b471515b56
grandmarathonGGJ2019@gmail.com <grandmarathonGGJ2019@gmail.com>
2019-01-26 14:42:24 +0800

    update physics

--- GGJ2019/Assets/Scripts/CustomPhysics.cs
+++ GGJ2019/Assets/Scripts/CustomPhysics.cs
@@ -3,78 +3,119 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CustomPhysics : MonoBehaviour {
+public class CustomPhysics : MonoBehaviour
+{
 
     private Transform target;
 
-    private Vector3 MULTIPLIER = new Vector3(1f, 1f , 1f);
+    private Vector3 MULTIPLIER = new Vector3(1f, 1f, 1f);
     private Vector3 GRAVITY = new Vector3(0, 0.3f, 0);
-    private Vector3 RESISTANCE = new Vector3(0.2f, 0, 0);
+    private float RESISTANCE = 0.5f;
+
+    private Vector3 initAcceleration;
+
+
+    Vector3 MAX_PUSH = new Vector3(4, 3, 0);
 
     Vector3 velocity = new Vector3(0, 0, 0);
     Vector3 acceleration = new Vector3(0, -1f, 0);
 
-    float mockLimitPositionY = 0;   //TEST
 
-	// Use this for initialization
-	void Start () {
+    public bool grounded = false;
+    public Vector3 tmp = new Vector3(0,0,0); 
+
+    // Use this for initialization
+    void Start()
+    {
         this.target = gameObject.transform;
-	}
+    }
 
 
-	// Update is called once per frame
-	void Update () {
-
-        if(Input.GetMouseButtonDown(0)){
-            this.Push(new Vector3(4, 3, 2));
-        }
-
-        if(acceleration.y > -1f){
-            acceleration = new Vector3(acceleration.x, acceleration.y - GRAVITY.y, acceleration.z);
-        }
-        if (acceleration.x > RESISTANCE.x)
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.grounded == true)
         {
-            acceleration = new Vector3(acceleration.x - RESISTANCE.x, acceleration.y, acceleration.z);
+            if (Input.GetMouseButtonDown(0))
+            {
+                this.Push(new Vector3(10, 2, 2));
+            }
+
         }
 
-        this.velocity += new Vector3 (acceleration.x * MULTIPLIER.x, acceleration.y * MULTIPLIER.y, acceleration.z * MULTIPLIER.z) * Time.deltaTime;
+        if (this.grounded == false)
+        {
+            if (acceleration.y > -1f)
+            {
+                acceleration = new Vector3(acceleration.x, Mathf.Max(acceleration.y - GRAVITY.y, -1), acceleration.z);
+            }
+        }else{
+            Debug.Log("Grounded");
+            if (velocity.x > 0)
+            {
+                acceleration = new Vector3(acceleration.x - RESISTANCE, acceleration.y, acceleration.z);
+            }
+        }
+        this.velocity += new Vector3(acceleration.x * MULTIPLIER.x, acceleration.y * MULTIPLIER.y, acceleration.z * MULTIPLIER.z) * Time.deltaTime;
+        this.velocity = new Vector3(Mathf.Max(this.velocity.x, 0), (grounded ? this.target.position.y >= tmp.y ?  tmp.y - this.target.position.y / 4 : 0 : this.velocity.y), this.velocity.z);
 
-
+        if(this.grounded == false){
+            DetectFloor();
+        }
+        //Debug.Log(this.velocity);
         this.target.position += velocity;
+    }
 
 
+    public float getResistance(float velocityx){
+        return 0;
+    }
+
+    private void DetectFloor(){
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity, LayerMask.GetMask("Ground")))
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
-            Debug.Log("Did Hit");
+
+            if (hit.distance >= this.target.position.y + this.velocity.y)
+            {
+                OnFloorHit(hit);
+
+            }
         }
         else
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * 1000, Color.white);
             Debug.Log("Did not Hit");
         }
+    }
 
 
-        if (target.position.y < mockLimitPositionY)
-        {
-            target.position = new Vector3(target.position.x, mockLimitPositionY, target.position.z);
-            velocity = Vector3.zero;
-            acceleration = Vector3.zero;
-        }
-	}
+    private void OnFloorHit(RaycastHit hit){
+        this.grounded = true;
+        velocity = new Vector3(velocity.x, 0 ,velocity.z);
+        acceleration = Vector3.zero;
+        tmp = hit.point;
+    }
 
-
-
-
-	public void Push(Vector3 targetPosition){
+	public void Push(Vector3 targetAcceleration){
         Debug.Log("Push");
-
-        velocity = new Vector3(0, 0, 0);
-        acceleration = new Vector3(targetPosition.x * MULTIPLIER.x, targetPosition.y * MULTIPLIER.y, 0);
+        this.grounded = false;
+        this.velocity = new Vector3(targetAcceleration.x * MULTIPLIER.x, targetAcceleration.y * MULTIPLIER.y, 0) * Time.deltaTime;
+        this.acceleration = new Vector3(0, targetAcceleration.y * MULTIPLIER.y, 0);
+        this.initAcceleration = this.acceleration;
     }
 
-    public void Rebounce(){
-        
-    }
+
+    //public void Bounce(){
+    //    Rebounce(0.3f);
+    //}
+
+    //public void Rebounce(float rebounceMultiplier){
+    //    if (rebounceMultiplier > 0)
+    //    {
+    //        acceleration = initAcceleration * rebounceMultiplier;
+    //        rebounceMultiplier -= 0.1f;
+    //    }
+    //}
 }
