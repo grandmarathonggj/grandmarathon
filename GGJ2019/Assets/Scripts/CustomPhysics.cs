a57611e35f8c3f75f9a6c021f27a574bf3c5f093
grandmarathonGGJ2019@gmail.com <grandmarathonGGJ2019@gmail.com>
2019-01-26 18:00:12 +0800

    update

--- GGJ2019/Assets/Scripts/CustomPhysics.cs
+++ GGJ2019/Assets/Scripts/CustomPhysics.cs
@@ -12,20 +12,20 @@ public class CustomPhysics : MonoBehaviour
     public float GRAVITY = 0.3f;
     public float RESISTANCE = 0.3f;
 
-	public float MAX_AX = 15f;
+    public float MAX_AX = 15f;
     public float MAX_AY = 2.5f;
 
-	private Vector3 initAcceleration;
+    private Vector3 initAcceleration;
 
     Vector3 MAX_PUSH = new Vector3(4, 3, 0);
 
     Vector3 velocity = new Vector3(0, 0, 0);
     Vector3 acceleration = new Vector3(0, -1f, 0);
 
-
     public bool grounded = false;
-    private bool controllable = true;
-
+    public bool collided = false;
+    public bool controllable = true;
+    private Vector3 dragAngle = new Vector3(0,0,0);
     // Use this for initialization
     void Start()
     {
@@ -51,31 +51,52 @@ public class CustomPhysics : MonoBehaviour
             {
                 acceleration = new Vector3(acceleration.x, Mathf.Max(acceleration.y - GRAVITY, -1), acceleration.z);
             }
-        }else{
-            if (velocity.x > 0)
+        }
+        else
+        {
+            if ((velocity.x > 0 && dragAngle.x > 0) || (velocity.x < 0 && dragAngle.x < 0))
             {
-                acceleration = new Vector3(acceleration.x - RESISTANCE, acceleration.y, acceleration.z);
+                acceleration = new Vector3(acceleration.x - RESISTANCE * dragAngle.x, acceleration.y, acceleration.z);
+            }else{
+                velocity = new Vector3(0, velocity.y, velocity.z);
+                acceleration =new Vector3(0, acceleration.y, acceleration.z);
+            }
+
+            if ((velocity.z > 0 && dragAngle.z > 0) || (velocity.z < 0 && dragAngle.z < 0))
+            {
+                acceleration = new Vector3(acceleration.x, acceleration.y, acceleration.z - RESISTANCE * dragAngle.z);
+            }else{
+                velocity = new Vector3(velocity.x, velocity.y, 0);
+                acceleration = new Vector3(acceleration.x, acceleration.y, 0);
             }
         }
-        this.velocity += new Vector3(acceleration.x * MULTIPLIER.x, acceleration.y * MULTIPLIER.y, acceleration.z * MULTIPLIER.z) * Time.deltaTime;
-        float magnitute = Mathf.Max(this.velocity.x, 0);
-        float vx = magnitute * Mathf.Cos(transform.eulerAngles.y - 90);
-        float vz = magnitute * Mathf.Sin(transform.eulerAngles.y - 90);
 
-        this.velocity = new Vector3(vx, this.velocity.y, vz);
-       
-        if(velocity == Vector3.zero){
+        this.velocity += new Vector3(acceleration.x * MULTIPLIER.x, acceleration.y * MULTIPLIER.y, acceleration.z * MULTIPLIER.z) * Time.deltaTime;
+        //float magnitute = Mathf.Max(this.velocity.x, 0);
+        //Debug.Log(magnitute)
+        //float vx = magnitute * Mathf.Cos(transform.eulerAngles.y - 90);
+        //float vz = magnitute * Mathf.Sin(transform.eulerAngles.y - 90);
+
+        //this.velocity = new Vector3(vx, this.velocity.y, vz);
+
+        if (velocity == Vector3.zero)
+        {
             controllable = true;
-        }else{
+            collided = false;
+        }
+        else
+        {
             controllable = false;
         }
 
-        if(this.grounded == false){
+        if (this.grounded == false)
+        {
             DetectFloor();
         }
         //Debug.Log(this.velocity);
-
         DetectFront();
+
+
         this.target.position += velocity;
     }
 
@@ -86,11 +107,18 @@ public class CustomPhysics : MonoBehaviour
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, LayerMask.GetMask("Ground")))
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.green);
-            if (this.velocity.x < 0)
+
+            float diffX = Mathf.Abs(hit.point.x - this.target.position.x);
+            float diffZ = Mathf.Abs(hit.point.z - this.target.position.z);
+
+            if (Mathf.Abs(this.velocity.x) > diffX)
             {
-                if (hit.point.x <= this.target.position.x + this.velocity.x)
+                if (collided == false)
                 {
-                    OnWallHit(hit);
+                    this.target.position = new Vector3(hit.point.x, this.target.position.y, this.target.position.z);
+                    velocity = new Vector3(-velocity.x, velocity.y, -velocity.z);
+                    acceleration = new Vector3(-acceleration.x, acceleration.y, -acceleration.z);
+                    collided = true;
                 }
             }
         }
@@ -101,14 +129,16 @@ public class CustomPhysics : MonoBehaviour
         }
     }
 
-    private void OnWallHit(RaycastHit hit)
+    //private void OnWallHit(RaycastHit hit)
+    //{
+    //    this.target.position = new Vector3(hit.point.x, this.target.position.y, this.target.position.z);
+    //    velocity = new Vector3(-velocity.x, velocity.y, velocity.z);
+
+    //}
+
+
+    private void DetectFloor()
     {
-        this.target.position = new Vector3(hit.point.x, this.target.position.y, this.target.position.z);
-        velocity = new Vector3(-velocity.x, velocity.y, velocity.z);
-    }
-
-
-    private void DetectFloor(){
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity, LayerMask.GetMask("Ground")))
         {
@@ -126,26 +156,26 @@ public class CustomPhysics : MonoBehaviour
     }
 
 
-    private void OnFloorHit(RaycastHit hit){
+    private void OnFloorHit(RaycastHit hit)
+    {
         this.grounded = true;
         this.target.position = new Vector3(this.target.position.x, hit.point.y, this.target.position.z);
-        velocity = new Vector3(velocity.x, 0 ,velocity.z);
+        velocity = new Vector3(velocity.x, 0, velocity.z);
         acceleration = Vector3.zero;
     }
 
-    public void Push(Vector3 dragAngle, float dragDistance){
-        float ax = Mathf.Lerp(0, MAX_AX, dragDistance);
+    public void Push(Vector3 dragAngle, float dragDistance)
+    {
+        this.dragAngle = dragAngle;
+        float ax = Mathf.Lerp(0, MAX_AX, dragDistance) * dragAngle.x;
         float ay = Mathf.Lerp(0, MAX_AY, dragDistance);
-        float az = 0;
-
-
+        float az = Mathf.Lerp(0, MAX_AX, dragDistance) * dragAngle.z;
 
         Vector3 targetAcceleration = new Vector3(ax, ay, az);
         Debug.Log("Push");
         this.grounded = false;
-        this.velocity = new Vector3(targetAcceleration.x * MULTIPLIER.x, targetAcceleration.y * MULTIPLIER.y, 0) * Time.deltaTime;
+        this.velocity = new Vector3(targetAcceleration.x * MULTIPLIER.x, targetAcceleration.y * MULTIPLIER.y, targetAcceleration.z * MULTIPLIER.z) * Time.deltaTime;
         this.acceleration = new Vector3(0, targetAcceleration.y * MULTIPLIER.y, 0);
-        this.initAcceleration = this.acceleration;
     }
 
 
