e14d233c6ccbea53cf957c42cd495f6afb760b59
grandmarathonGGJ2019@gmail.com <grandmarathonGGJ2019@gmail.com>
2019-01-27 09:28:15 +0800

    update star VFX

--- GGJ2019/Assets/Scripts/VFXController.cs
+++ GGJ2019/Assets/Scripts/VFXController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class VFXController : MonoBehaviour {
 
     public GameObject collidingVFX;
+    public GameObject starCollectVFX;
 
 	// Use this for initialization
 	void Start () {
@@ -19,6 +20,12 @@ public class VFXController : MonoBehaviour {
 
     public void TriggerCollide(Vector3 location){
         GameObject particle = ((GameObject)Instantiate(collidingVFX));
+        particle.transform.position = location;
+    }
+
+    public void TriggerStar(Vector3 location)
+    {
+        GameObject particle = ((GameObject)Instantiate(starCollectVFX));
         particle.transform.position = location;
     }
 }
